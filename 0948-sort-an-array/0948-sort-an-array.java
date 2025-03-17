6745217f5a8ce37f20658a572c39bca531b632af
Tushar Koundal <154369254+rex69690@users.noreply.github.com>
2025-03-17 23:14:13 +0530

    Time: 20 ms (78.02%), Space: 57.7 MB (9.96%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int res [] = new int[nums.length];
+
+        int temp = 0;
+
+         Arrays.sort(nums);
+         return nums;
+        
+    }
+}

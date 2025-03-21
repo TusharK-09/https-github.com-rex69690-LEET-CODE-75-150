bfe2a86007150868504ef59008741f2fca733bfb
Tushar Koundal <154369254+rex69690@users.noreply.github.com>
2025-03-21 10:58:16 +0530

    Time: 1 ms (82.98%), Space: 41.9 MB (45.11%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,28 +1,34 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int i = 0 , j = 0;
+        String ans = "";
         StringBuilder sb = new StringBuilder();
-
-        String res = "";
-        String longerString ="";
-        
-        int min = Math.min(word1.length() , word2.length());
-
-        if(min == word1.length()){
-            longerString = word2;
-        }
-        else{
-            longerString = word1;
+        while(i<n && j<m ){
+            sb.append(word1.charAt(i));
+            i++;
+            sb.append(word2.charAt(j));
+            j++;
         }
 
-        for(int i = 0; i < min; i++){
-            
-                sb.append(word1.charAt(i));
-                sb.append(word2.charAt(i));
-            
-        }
-        sb.append(longerString.substring(min));
+        //agr meri word2 string bdi hai word1 se
+        //toh uske bache hue element add krne hai
 
-        res = sb.toString();
-        return res;
+        while(j<m){
+            sb.append(word2.charAt(j));
+            j++;
+        }
+
+           //agr meri word1 string bdi hai word2 se
+        //toh uske bache hue element add krne hai
+
+        while(i<n){
+            sb.append(word1.charAt(i));
+            i++;
+        }
+
+
+        return sb.toString();
     }
 }

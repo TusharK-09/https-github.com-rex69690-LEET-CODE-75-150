91ef697c74e988214b529d6fd7a75dd6c359f5e4
Tushar Koundal <154369254+rex69690@users.noreply.github.com>
2025-03-21 11:35:18 +0530

    Time: 1 ms (71.4%), Space: 42.4 MB (33.05%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -15,10 +15,7 @@ class Solution {
     }
 
       public int gcd (int num1 , int num2){
-        if(num2 == 0){
-            return num1;
-        }
-          return gcd(num2,num1 % num2);
+     return (num2 == 0 ? num1 : gcd(num2,num1%num2));
         
      }
 
